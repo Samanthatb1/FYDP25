7ca78da031dbe222ee61090916ca29367e19e95f
Samantha Grieco <samtb1@outlook.com>
2026-01-16 13:15:56 -0500

    display

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/jenny/FYDP25
 
+# Set DISPLAY variable for GUI applications
+export DISPLAY=:0
+
 source src/venv/bin/activate 
 pip install -r requirements.txt
 python src/main.py
